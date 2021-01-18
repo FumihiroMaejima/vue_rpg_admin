c74d61f0a313d3956134493ccd0c96e533abf157
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-01-19 02:02:38 +0900

    add game prefix tp game tables

--- app/backend/database/migrations/2021_01_14_000000_create_game_model_table.php
+++ app/backend/database/migrations/2021_01_14_000000_create_game_model_table.php
@@ -14,9 +14,9 @@ class CreateGameModelTable extends Migration
     public function up()
     {
         /**
-         * ablity table
+         * game_ablity table
          */
-        Schema::create('ability', function (Blueprint $table) {
+        Schema::create('game_ability', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->string('target_column1', 255)->comment('対象カラム1');
@@ -29,9 +29,9 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * area table
+         * game_area table
          */
-        Schema::create('area', function (Blueprint $table) {
+        Schema::create('game_area', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->text('message')->comment('エリアの説明');
@@ -42,9 +42,9 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * diffense_equipment table
+         * game_diffense_equipment table
          */
-        Schema::create('diffense_equipment', function (Blueprint $table) {
+        Schema::create('game_diffense_equipment', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->text('message')->comment('装備品の説明');
@@ -59,9 +59,9 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * offence_equipment table
+         * game_offence_equipment table
          */
-        Schema::create('offence_equipment', function (Blueprint $table) {
+        Schema::create('game_offence_equipment', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->text('message')->comment('装備品の説明');
@@ -76,9 +76,9 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * enemy table
+         * game_enemy table
          */
-        Schema::create('enemy', function (Blueprint $table) {
+        Schema::create('game_enemy', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->integer('level')->default(1)->comment('レベル');
@@ -90,14 +90,14 @@ class CreateGameModelTable extends Migration
             $table->integer('magic')->default(0)->comment('魔力');
             // $table->integer('offence_equipment')->nullable()->comment('攻撃用装備');
             // s$table->integer('diffense_equipment')->nullable()->comment('守備用装備');
-            $table->foreignId('offence_equipment_id')->constrained('offence_equipment')->nullable()->comment('攻撃用装備');
-            $table->foreignId('diffense_equipment_id')->constrained('diffense_equipment')->nullable()->comment('守備用装備');
+            $table->foreignId('offence_equipment_id')->constrained('game_offence_equipment')->nullable()->comment('攻撃用装備');
+            $table->foreignId('diffense_equipment_id')->constrained('game_diffense_equipment')->nullable()->comment('守備用装備');
             // $table->integer('ability1')->nullable()->comment('能力1');
             // $table->integer('ability2')->nullable()->comment('能力2');
             // $table->integer('ability3')->nullable()->comment('能力3');
-            $table->foreignId('ability1_id')->constrained('ability')->nullable()->comment('能力1');
-            $table->foreignId('ability2_id')->constrained('ability')->nullable()->comment('能力2');
-            $table->foreignId('ability3_id')->constrained('ability')->nullable()->comment('能力3');
+            $table->foreignId('ability1_id')->constrained('game_ability')->nullable()->comment('能力1');
+            $table->foreignId('ability2_id')->constrained('game_ability')->nullable()->comment('能力2');
+            $table->foreignId('ability3_id')->constrained('game_ability')->nullable()->comment('能力3');
             $table->text('item')->nullable()->comment('持ち物');
             $table->tinyInteger('event_only_flg')->default(0)->comment('イベントのみ出現フラグ');
             $table->integer('emcount_area_id')->comment('出現エリアID');
@@ -108,13 +108,13 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * character table
+         * game_character table
          */
-        Schema::create('character', function (Blueprint $table) {
+        Schema::create('game_character', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             // $table->integer('enemy_id')->nullable()->comment('敵キャラクターID');
-            $table->foreignId('enemy_id')->constrained('enemy')->nullable()->comment('敵キャラクターID');
+            $table->foreignId('enemy_id')->constrained('game_enemy')->nullable()->comment('敵キャラクターID');
             $table->string('image_name', 255)->unique()->comment('イメージネーム');
             $table->string('image_url', 255)->unique()->comment('イメージパス');
             $table->tinyInteger('dead_flg')->default(0)->comment('死亡済みフラグ');
@@ -123,13 +123,13 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * event table
+         * game_event table
          */
-        Schema::create('event', function (Blueprint $table) {
+        Schema::create('game_event', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             // $table->integer('area_id')->comment('エリア名ID');
-            $table->foreignId('area_id')->constrained('area')->comment('エリア名ID'); // テーブル名とid名が一致する場合
+            $table->foreignId('area_id')->constrained('game_area')->comment('エリア名ID'); // テーブル名とid名が一致する場合
             $table->tinyInteger('character_id1')->comment('キャラクターID1');
             $table->tinyInteger('character_id2')->comment('キャラクターID2');
             $table->timestamps();
@@ -137,9 +137,9 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * item table
+         * game_item table
          */
-        Schema::create('item', function (Blueprint $table) {
+        Schema::create('game_item', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->text('message')->comment('アイテムの説明');
@@ -154,9 +154,9 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * title table
+         * game_title table
          */
-        Schema::create('title', function (Blueprint $table) {
+        Schema::create('game_title', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->text('message')->comment('タイトルの説明');
@@ -165,9 +165,9 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * player table
+         * game_player table
          */
-        Schema::create('player', function (Blueprint $table) {
+        Schema::create('game_player', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->comment('名前');
             $table->integer('level')->default(1)->comment('レベル');
@@ -179,16 +179,16 @@ class CreateGameModelTable extends Migration
             $table->integer('magic')->default(0)->comment('魔力');
             // $table->integer('offence_equipment')->nullable()->comment('攻撃用装備');
             // $table->integer('diffense_equipment')->nullable()->comment('守備用装備');
-            $table->foreignId('offence_equipment_id')->constrained('offence_equipment')->nullable()->comment('攻撃用装備');
-            $table->foreignId('diffense_equipment_id')->constrained('diffense_equipment')->nullable()->comment('守備用装備');
+            $table->foreignId('offence_equipment_id')->constrained('game_offence_equipment')->nullable()->comment('攻撃用装備');
+            $table->foreignId('diffense_equipment_id')->constrained('game_diffense_equipment')->nullable()->comment('守備用装備');
             // $table->integer('ability1')->nullable()->comment('能力1');
             // $table->integer('ability2')->nullable()->comment('能力2');
             // $table->integer('ability3')->nullable()->comment('能力3');
-            $table->foreignId('ability1_id')->constrained('ability')->nullable()->comment('能力1');
-            $table->foreignId('ability2_id')->constrained('ability')->nullable()->comment('能力2');
-            $table->foreignId('ability3_id')->constrained('ability')->nullable()->comment('能力3');
+            $table->foreignId('ability1_id')->constrained('game_ability')->nullable()->comment('能力1');
+            $table->foreignId('ability2_id')->constrained('game_ability')->nullable()->comment('能力2');
+            $table->foreignId('ability3_id')->constrained('game_ability')->nullable()->comment('能力3');
             // $table->integer('title_id')->default(1)->comment('肩書き');
-            $table->foreignId('title_id')->constrained('title')->default(1)->comment('肩書き');
+            $table->foreignId('title_id')->constrained('game_title')->default(1)->comment('肩書き');
             $table->text('item')->nullable()->comment('持ち物');
             $table->tinyInteger('special_item_flg1')->default(0)->comment('keyアイテム取得フラグ1');
             $table->tinyInteger('special_item_flg2')->default(0)->comment('keyアイテム取得フラグ2');
@@ -202,12 +202,12 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * player_log table
+         * game_player_log table
          */
-        Schema::create('player_log', function (Blueprint $table) {
+        Schema::create('game_player_log', function (Blueprint $table) {
             $table->id();
             // $table->integer('player_id')->comment('プレイヤーID');
-            $table->foreignId('player_id')->constrained('player')->comment('プレイヤーID');
+            $table->foreignId('player_id')->constrained('game_player')->comment('プレイヤーID');
             $table->string('function', 255)->comment('実行ファンクション');
             $table->string('status', 255)->comment('ステータス');
             $table->timestamp('action_time')->comment('実行日時');
@@ -216,31 +216,31 @@ class CreateGameModelTable extends Migration
         });
 
         /**
-         * player_item_list table
+         * game_player_item_list table
          */
-        Schema::create('player_item_list', function (Blueprint $table) {
+        Schema::create('game_player_item_list', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('player_id')->constrained('player')->comment('プレイヤーID');
-            $table->foreignId('content1')->constrained('item')->nullable()->comment('コンテンツ1');
-            $table->foreignId('content2')->constrained('item')->nullable()->comment('コンテンツ2');
-            $table->foreignId('content3')->constrained('item')->nullable()->comment('コンテンツ3');
-            $table->foreignId('content4')->constrained('item')->nullable()->comment('コンテンツ4');
-            $table->foreignId('content5')->constrained('item')->nullable()->comment('コンテンツ5');
-            $table->foreignId('content6')->constrained('item')->nullable()->comment('コンテンツ6');
-            $table->foreignId('content7')->constrained('item')->nullable()->comment('コンテンツ7');
-            $table->foreignId('content8')->constrained('item')->nullable()->comment('コンテンツ8');
-            $table->foreignId('content9')->constrained('item')->nullable()->comment('コンテンツ9');
-            $table->foreignId('content10')->constrained('item')->nullable()->comment('コンテンツ10');
-            $table->foreignId('content11')->constrained('item')->nullable()->comment('コンテンツ11');
-            $table->foreignId('content12')->constrained('item')->nullable()->comment('コンテンツ12');
-            $table->foreignId('content13')->constrained('item')->nullable()->comment('コンテンツ13');
-            $table->foreignId('content14')->constrained('item')->nullable()->comment('コンテンツ14');
-            $table->foreignId('content15')->constrained('item')->nullable()->comment('コンテンツ15');
-            $table->foreignId('content16')->constrained('item')->nullable()->comment('コンテンツ16');
-            $table->foreignId('content17')->constrained('item')->nullable()->comment('コンテンツ17');
-            $table->foreignId('content18')->constrained('item')->nullable()->comment('コンテンツ18');
-            $table->foreignId('content19')->constrained('item')->nullable()->comment('コンテンツ19');
-            $table->foreignId('content20')->constrained('item')->nullable()->comment('コンテンツ20');
+            $table->foreignId('player_id')->constrained('game_player')->comment('プレイヤーID');
+            $table->foreignId('content1')->constrained('game_item')->nullable()->comment('コンテンツ1');
+            $table->foreignId('content2')->constrained('game_item')->nullable()->comment('コンテンツ2');
+            $table->foreignId('content3')->constrained('game_item')->nullable()->comment('コンテンツ3');
+            $table->foreignId('content4')->constrained('game_item')->nullable()->comment('コンテンツ4');
+            $table->foreignId('content5')->constrained('game_item')->nullable()->comment('コンテンツ5');
+            $table->foreignId('content6')->constrained('game_item')->nullable()->comment('コンテンツ6');
+            $table->foreignId('content7')->constrained('game_item')->nullable()->comment('コンテンツ7');
+            $table->foreignId('content8')->constrained('game_item')->nullable()->comment('コンテンツ8');
+            $table->foreignId('content9')->constrained('game_item')->nullable()->comment('コンテンツ9');
+            $table->foreignId('content10')->constrained('game_item')->nullable()->comment('コンテンツ10');
+            $table->foreignId('content11')->constrained('game_item')->nullable()->comment('コンテンツ11');
+            $table->foreignId('content12')->constrained('game_item')->nullable()->comment('コンテンツ12');
+            $table->foreignId('content13')->constrained('game_item')->nullable()->comment('コンテンツ13');
+            $table->foreignId('content14')->constrained('game_item')->nullable()->comment('コンテンツ14');
+            $table->foreignId('content15')->constrained('game_item')->nullable()->comment('コンテンツ15');
+            $table->foreignId('content16')->constrained('game_item')->nullable()->comment('コンテンツ16');
+            $table->foreignId('content17')->constrained('game_item')->nullable()->comment('コンテンツ17');
+            $table->foreignId('content18')->constrained('game_item')->nullable()->comment('コンテンツ18');
+            $table->foreignId('content19')->constrained('game_item')->nullable()->comment('コンテンツ19');
+            $table->foreignId('content20')->constrained('game_item')->nullable()->comment('コンテンツ20');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -253,17 +253,17 @@ class CreateGameModelTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ability');
-        Schema::dropIfExists('area');
-        Schema::dropIfExists('diffense_equipment');
-        Schema::dropIfExists('offence_equipment');
-        Schema::dropIfExists('enemy');
-        Schema::dropIfExists('character');
-        Schema::dropIfExists('event');
-        Schema::dropIfExists('item');
-        Schema::dropIfExists('title');
-        Schema::dropIfExists('player');
-        Schema::dropIfExists('player_log');
-        Schema::dropIfExists('player_item_list');
+        Schema::dropIfExists('game_ability');
+        Schema::dropIfExists('game_area');
+        Schema::dropIfExists('game_diffense_equipment');
+        Schema::dropIfExists('game_offence_equipment');
+        Schema::dropIfExists('game_enemy');
+        Schema::dropIfExists('game_character');
+        Schema::dropIfExists('game_event');
+        Schema::dropIfExists('game_item');
+        Schema::dropIfExists('game_title');
+        Schema::dropIfExists('game_player');
+        Schema::dropIfExists('game_player_log');
+        Schema::dropIfExists('game_player_item_list');
     }
 }
