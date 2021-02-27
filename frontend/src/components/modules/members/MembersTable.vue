616a4b4c3562feaea878f57736cc753336fd5af2
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-02-27 23:51:57 +0900

    make members table.

---
+++ frontend/src/components/modules/members/MembersTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <app-table :items="members" :columnOptions="columnOptions" />
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  Ref,
+  PropType,
+  reactive,
+  computed,
+  provide,
+  inject
+} from 'vue'
+import AppTable from '@/components/parts/AppTable.vue'
+import {
+  tableSetting,
+  MembersType,
+  StateKey,
+  StateStore,
+  useState
+} from '@/services/members'
+import AuthApp from '@/plugins/auth/authApp'
+import { inversionFlag } from '@/util'
+import { ToastType } from '@/types/components/index'
+
+export default defineComponent({
+  name: 'MembersTable',
+  components: {
+    AppTable
+  },
+  setup() {
+    const toast = inject('toast') as ToastType
+    const columnOptions = reactive(tableSetting)
+    const loadingFlag = inject('circleLoading') as Ref<boolean>
+    const authApp = inject('authApp') as AuthApp
+    const membersService = inject('membersService') as StateStore
+
+    // computed
+    const members = computed((): MembersType[] => membersService().state.members)
+    console.log('membersService: ' + JSON.stringify(membersService, null, 2))
+
+    // created
+    const created = async () => {
+      inversionFlag(loadingFlag)
+      const response = await membersService().getMembersData(authApp.getHeaderOptions())
+      if (response.status !== 200) {
+        toast.add(membersService().getToastData())
+      }
+      /* console.log(
+        'membersService.getMembersData(): ' +
+          JSON.stringify(membersService.getMembersData(), null, 2)
+      ) */
+      inversionFlag(loadingFlag)
+    }
+    created()
+
+    // methods
+
+    return {
+      members,
+      columnOptions
+    }
+  }
+})
+</script>
